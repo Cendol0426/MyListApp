5e912892af0b3a2d59740ac72a49ddb2705e0846
Sean <zhisean@gmail.com>
2024-06-27 12:01:46 +0800

    Web Complete

--- experimentIf.py
+++ experimentIf.py
@@ -4,7 +4,7 @@ if not os.path.exists("experiments_I.txt"):
     with open("experiments_I.txt", "w") as file:
         pass
 
-def get_variables(filepath=r"C:\Users\chong\PycharmProjects\web_app1\experiments_I.txt"):
+def get_variables(filepath = r"C:\Users\chong\PycharmProjects\web_app1\experiments_I.txt"):
     """ Read a text file and return the list of items"""
     with open(filepath, 'r') as file_local:
         result_local = file_local.readlines()
